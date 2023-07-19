2fcbedba1e483858668ca2f0dc6d66d27441eb9a
dakooten <kentoyfueconcillo@gmail.com>
2023-07-20 00:08:23 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+import webpack from 'webpack'
+
+const nodeExternals = require('webpack-node-externals')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+import { resolve } from 'path'
+
+module.exports = {
+  generate: {
+    routes: ['404']
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'my-project',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  components: true,
+  plugins: [
+    '~/plugins/events.js',
+    '~/plugins/font-awesome.js',
+    '~/plugins/http.js',
+    '~/plugins/vue-smooth-picker.js',
+    '~/plugins/vue-croppa.js',
+    {src: '~/plugins/vue-sample-uploader.js', ssr: false},
+    '~/plugins/vue-js-modal',
+    {src: '~/plugins/localStorage.js', ssr: false },
+    '~/plugins/filters.js'
+  ],
+  css: [
+    '~/assets/style/app.styl',
+    '~/assets/style/helvetica/helvetica.css',
+    '~/assets/style/helvetica-neue/helvetica-neue.css'
+  ],
+  env: {
+    API_URL: 'http://localhost:5000/' // waiting for the api xd
+  },
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify'
+  ],
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/axios',
+    '@nuxtjs/moment',
+    '@nuxtjs/apollo',
+    'nuxt-material-design-icons',
+    ['@nuxtjs/component-cache', {
+      max: 10000,
+      maxAge: 1000 * 60 * 60
+    }],
+  ],
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'http://localhost:8000/api/admin/graphiql',
+      }
+    }
+  },
+    /*
+  ** If you  want to use static hosting use 'target: static' in order for the generate command to work properly.
+  */
+
+  // target: 'static',
+
+  target: 'server', /* server hosting, when using build command */
+  
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    extend (config, ctx) {
+      config.resolve.symlinks = false
+    },
+    postcss: null
+  }
+}
---
+++ pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid fill-height>
+    <no-ssr>
+    <v-layout wrap justify-center align-center>
+      <v-flex xs12 sm8 md5 lg3>
+        <v-card class="elevation-12 px-2 pt-10">
+          <v-text-field
+            outlined
+            v-model="form.first_name"
+            label="first name"
+            prepend-icon="person"
+            class="px-2"/>
+            <v-text-field
+            outlined
+            v-model="form.last_name"
+            label="last name"
+            prepend-icon="person"
+            class="px-2"/>
+          <v-text-field
+            outlined
+            v-model="form.username"
+            label="username"
+            prepend-icon="person"
+            class="px-2"/>
+          <v-text-field
+            outlined
+            v-model="form.password"
+            label="Password"
+            prepend-icon="lock"
+            class="px-2"
+            type="password"/>
+
+          <v-btn block depressed @click="login" class="white--text" color="#fbb730">
+            register
+          </v-btn>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    </no-ssr>
+  </v-container>
+</template>
+<script>
+  import Global from '~/plugins/mixins/global'
+  import axios from 'axios'
+  export default {
+    mixins: [Global],
+    data: () => ({
+      form: {}
+    }),
+    methods: {
+      login () {
+        axios.post('http://localhost:5000/register', this.form).then(data => {
+          console.log(data)
+        })
+      }
+    }
+  }
+</script>
