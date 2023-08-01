0b17721a250e973639e66d0cabe396bf81cc7889
kent <kentoyfueconcillo@gmail.com>
2023-08-01 11:39:40 +0800

    added search in admin, added user name and last name upon return

--- pages/admin/index/index.vue
+++ pages/admin/index/index.vue
@@ -120,6 +120,9 @@
                 <thead>
                   <tr>
                     <th class="text-left">
+                      Requestor Name
+                    </th>
+                    <th class="text-left">
                       Name Reported
                     </th>
                     <th class="text-left">
@@ -138,15 +141,30 @@
                     v-for="(item, index) in requests"
                     :key="index"
                   >
+                    <td>{{ item.requested_by_user ? item.requested_by_user.first_name : '' }} {{ item.requested_by_user? item.requested_by_user.last_name : '' }}</td>
                     <td>{{ item.name_reported }}</td>
                     <td>{{ item.reason }}</td>
-                    <td>{{ !isNull(item.schedule_hearing) ? item.schedule_hearing : 'Waiting' }}</td>
-                    <td><v-btn @click="getData(item)">Set Schedule</v-btn></td>
+                    <td>{{ !isNull(item.schedule_hearing) ? parseDate(item.schedule_hearing) : 'Waiting' }}</td>
+                    <td><v-btn @click="getData(item)"> {{ !isNull(item.schedule_hearing) ? 'Change Schedule' : 'Set Schedule'}}</v-btn></td>
                   </tr>
                 </tbody>
               </template>
             </v-simple-table>
             </v-card>
+
+            
+            <v-dialog width="300" v-model="datedialog">
+              <v-card>
+                <v-col
+                  cols="12"
+                  lg="12"
+                  sm="6"
+                >
+                  <v-date-picker style="width: 100%;" v-model="picker"></v-date-picker>
+                  <v-btn @click="selectdate(picker)">Select</v-btn>
+                </v-col>
+              </v-card>
+            </v-dialog>
 
             <!-- <v-btn @click="sendsms()">
               send sms
@@ -163,18 +181,21 @@
   import { mapGetters } from 'vuex'
   import axios from 'axios'
   import _ from 'lodash'
+  import moment from 'moment'
 
   export default {
     data: () => ({
       requests: [],
       dialog: false,
+      datedialog: false,
       form: {},
-      search: ''
+      search: '',
+      picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      selectedconcern: {}
     }),
     methods: {
       async getallreports () {
         await axios.get('http://localhost:5000/get-all-concerns').then(data => {
-          console.log(data)
           this.requests = data.data
         })
       },
@@ -186,8 +207,21 @@
       isNull (param) {
         return _.isNull(param)
       },
+      parseDate (param) {
+        return moment(param).format('LL')
+      },
       getData (param) {
+        this.datedialog = true
+        this.selectedconcern = param
         console.log(param)
+      },
+      async selectdate (param) {
+        let fparam = this.selectedconcern
+        this.$set(fparam, 'schedule_hearing', param)
+        await axios.post('http://localhost:5000/update-report-user', fparam).then(data => {
+          this.getallreports()
+          this.datedialog = false
+        })
       },
       async sendsms () {
         let param = {
